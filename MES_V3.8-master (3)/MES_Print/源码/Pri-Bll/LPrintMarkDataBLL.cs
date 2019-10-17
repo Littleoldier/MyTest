6aab0895dc990884bc5a737d0decf6a62009fc49
Littleoldier <46140690+Littleoldier@users.noreply.github.com>
2019-10-17 18:15:14 +0800

    新需求 IMEI多号段设置、RFID扫入框

---
+++ MES_V3.8-master (3)/MES_Print/源码/Pri-Bll/LPrintMarkDataBLL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LPrintMarkData.Param.Pri_Dal;
+using Print_Message;
+
+namespace LPrintMarkData.Param.Pri_BLL
+{
+    class LPrintMarkDataBLL
+    {
+        private LPrintMarkDataDAL LPMDD = new LPrintMarkDataDAL();
+
+
+        //public bool CheckIMEIBLL(string IMEInumber)
+        //{
+        //    if (LPMDD.CheckIMEINumberDAL(IMEInumber) > 0)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+
+
+        public bool CheckIMEIBLL(string IMEInumber)
+        {
+            if (LPMDD.CheckIMEIDAL(IMEInumber) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public List<PrintMessage> CheckRangeIMEIBLL(string StarIMEI, string EndIMEI)
+        {
+            return LPMDD.CheckRangeIMEIDAL(StarIMEI, EndIMEI);
+        }
+    }
+}
---
+++ MES_V3.8-master (3)/MES_Print/源码/Pri-Dal/LPrintMarkDataDAL.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Print_Message;
+using System.Configuration;
+
+namespace LPrintMarkData.Param.Pri_Dal
+{
+    class LPrintMarkDataDAL
+    {
+        private static string conStr = ConfigurationManager.ConnectionStrings["conn1"].ConnectionString;
+
+
+        //检查IMEI号是否存在
+        public int CheckIMEINumberDAL(string IMEINumber)
+        {
+            SqlConnection conn1 = new SqlConnection(conStr);
+            conn1.Open();
+            using (SqlCommand command = conn1.CreateCommand())
+            {
+                command.CommandText = "SELECT IMEI FROM dbo.[LPrintMarkData] WHERE IMEI='" + IMEINumber + "'";
+                SqlDataReader dr = command.ExecuteReader();
+                while (dr.Read())
+                {
+                    return 1;
+                }
+                return 0;
+            }
+        }
+
+         //范围检查IMEI号是否存在，存在返回IMEI，否则返回0
+        public List<PrintMessage> CheckRangeIMEIDAL(string StarIMEI, string EndIMEI)
+        {
+            List<PrintMessage> pm = new List<PrintMessage>();
+            using (SqlConnection conn1 = new SqlConnection(conStr))
+            {
+                conn1.Open();
+                using (SqlCommand command = conn1.CreateCommand())
+                {
+                    command.CommandText = "SELECT IMEI FROM dbo.[LPrintMarkData] WHERE (IMEI>='" + StarIMEI + "' AND IMEI<='" + EndIMEI + "')";
+                    SqlDataReader dr = command.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        pm.Add(new PrintMessage()
+                        {
+                            IMEI = dr.GetString(0)
+                        });
+                    }
+                    return pm;
+                }
+            }
+        }
+
+        //检查IMEI号是否存在，存在返回1，否则返回0
+        public int CheckIMEIDAL(string IMEInumber)
+        {
+            using (SqlConnection conn1 = new SqlConnection(conStr))
+            {
+                conn1.Open();
+                using (SqlCommand command = conn1.CreateCommand())
+                {
+                    command.CommandText = "SELECT ID FROM dbo.LPrintMarkData WHERE IMEI='" + IMEInumber + "'";
+                    string dr = Convert.ToString(command.ExecuteScalar());
+                    if (dr != "")
+                    {
+                        return 1;
+                    }
+                    return 0;
+                }
+            }
+        }
+    }
+}
